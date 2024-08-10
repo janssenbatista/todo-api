c1c580db25d9ca02d566894cba8b2d00612f9863
Janssen Batista <batistajanssen.dev@gmail.com>
2024-08-09 22:36:48 -0300

    test: add test to endpoint that list all tasks

--- src/test/java/dev/janssenbatista/todoapi/controllers/dtos/TaskControllerTest.java
+++ src/test/java/dev/janssenbatista/todoapi/controllers/dtos/TaskControllerTest.java
@@ -5,18 +5,19 @@ import dev.janssenbatista.todoapi.controllers.TaskController;
 import dev.janssenbatista.todoapi.entities.TaskEntity;
 import dev.janssenbatista.todoapi.exceptions.BadRequestException;
 import dev.janssenbatista.todoapi.services.TaskService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TaskController.class)
 class TaskControllerTest {
@@ -35,22 +36,28 @@ class TaskControllerTest {
         var dto = new CreateTaskDto("task title", "task description");
         var createdTask = new TaskEntity(1L, dto.title(), dto.description(), false);
         when(taskService.save(dto)).thenReturn(createdTask);
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/tarefas")
+        mockMvc.perform(post("/api/tarefas")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsString(dto)))
-                .andExpect(MockMvcResultMatchers.status().isCreated());
+                .andExpect(status().isCreated());
     }
 
     @Test
     public void shouldNotCreateATaskAndReturn400() throws Exception {
         var dto = new CreateTaskDto("task title", "task description");
         when(taskService.save(dto)).thenThrow(new BadRequestException("Essa tarefa já existe"));
-        var response = mockMvc.perform(MockMvcRequestBuilders.post("/api/tarefas")
+        var response = mockMvc.perform(post("/api/tarefas")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(mapper.writeValueAsString(dto)))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(status().isBadRequest())
                 .andReturn();
         assertThat(response.getResponse().getContentAsString()).isEqualTo("Essa tarefa já existe");
+    }
+
+    @Test
+    public void shouldListAllTasks() throws Exception {
+        mockMvc.perform(get("/api/tarefas"))
+                .andExpect(status().isOk());
     }
 
 }

6960c9e2ba5476d24d6561fc7b1fd8ba9b0e5526
Janssen Batista <batistajanssen.dev@gmail.com>
2024-08-09 22:33:23 -0300

    feat: add endpoint to list all tasks

--- src/main/java/dev/janssenbatista/todoapi/controllers/TaskController.java
+++ src/main/java/dev/janssenbatista/todoapi/controllers/TaskController.java
@@ -3,6 +3,9 @@ package dev.janssenbatista.todoapi.controllers;
 import dev.janssenbatista.todoapi.controllers.dtos.CreateTaskDto;
 import dev.janssenbatista.todoapi.entities.TaskEntity;
 import dev.janssenbatista.todoapi.services.TaskService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,5 +23,12 @@ public class TaskController {
     @PostMapping()
     public ResponseEntity<TaskEntity> createTask(@RequestBody CreateTaskDto taskDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(taskService.save(taskDto));
+    }
+
+    @GetMapping()
+    public ResponseEntity<Page<TaskEntity>> getAllTasks(@RequestParam(name = "pageNumber", defaultValue = "0") int pageNumber,
+                                            @RequestParam(name = "pageSize", defaultValue = "10") int pageSize) {
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        return ResponseEntity.ok(taskService.listAll(pageable));
     }
 }
